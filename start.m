8ddf44ac64b44385ed5a9fb1bffb6ca0fdf6b860
zmzhang <zmzhang@csu.edu.cn>
2015-11-09 22:34:42 +0800

    add star.m code

---
+++ start.m
@@ -0,0 +1,7 @@
+currentDepth = 1; % get the supper path of the current path
+currPath = fileparts(cd);% get current path
+fsep = filesep;
+pos_v = strfind(cd,fsep);
+p = currPath(1:pos_v(length(pos_v)-currentDepth+1)-1); % -1: delete the last character '/' or '\' 
+codepath=[p '\' 'code'];
+path(path,codepath);
