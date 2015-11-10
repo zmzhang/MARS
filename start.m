c40179f3ce5faa4e4e8be736932b086c35faa02c
zmzhang <zmzhang@csu.edu.cn>
2015-11-10 09:34:08 +0800

    change mars_main function

--- start.m
+++ start.m
@@ -5,4 +5,4 @@ pos_v = strfind(cd,fsep);
 p = currPath(1:pos_v(length(pos_v)-currentDepth+1)-1); % -1: delete the last character '/' or '\' 
 codepath=[p '\' 'code'];
 path(path,codepath);
-mars_main('MSRT_input','p',30,5,0.9);
+mars_main('MSRT_input','p',30);
